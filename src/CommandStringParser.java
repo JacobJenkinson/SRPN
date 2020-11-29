0ed38dc2afada8329fd2764a517a37c984a06d8d
Jacob Jenkinson <jacob.jenkinson1@gmail.com>
2020-11-29 17:49:35 +0000

    Support Multiline comments

--- src/CommandStringParser.java
+++ src/CommandStringParser.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -5,11 +6,13 @@ import java.util.stream.Stream;
 
 public class CommandStringParser {
 
+    private boolean isNotInCommendMode = true;
+
     public List<String> parseCommand(final String command) {
         if (command.isBlank()) {
             return Collections.emptyList();
         } else {
-            final String possibleSeparators = "-rd+*%^/";
+            final String possibleSeparators = "rd+*%^/=";
             final String regex = "(?<=[" + possibleSeparators + "])|(?=[" + possibleSeparators + "])| ";
             final List<String> parsedCommands = Stream.of(command.split(regex))
                     .filter(cmd -> !cmd.isBlank())
@@ -19,11 +22,42 @@ public class CommandStringParser {
     }
 
     private List<String> removeComments(final List<String> commandArray) {
-        if (commandArray.contains("#")) {
-            return commandArray.subList(0, commandArray.indexOf("#"));
+        final List<Integer> commentPositions = findCommandPositions(commandArray, "#");
+        if (commentPositions.size() > 0) {
+            final List<String> nonCommentCommands = new ArrayList<>();
+            if (isNotInCommendMode) {
+                nonCommentCommands.addAll(commandArray.subList(0, commentPositions.get(0)));
+            }
+            isNotInCommendMode = !isNotInCommendMode;
+
+
+            for (int i = 0; i < commentPositions.size() - 1; i++) {
+                final List<String> subList = commandArray.subList(commentPositions.get(i) + 1, commentPositions.get(i + 1));
+                if (isNotInCommendMode) {
+                    nonCommentCommands.addAll(subList);
+                }
+                isNotInCommendMode = !isNotInCommendMode;
+            }
+
+            if (isNotInCommendMode) {
+                nonCommentCommands.addAll(commandArray.subList(commentPositions.get(commentPositions.size() - 1) + 1, commandArray.size()));
+            }
+
+            return nonCommentCommands;
         } else {
-            return commandArray;
+            return isNotInCommendMode ? commandArray : Collections.emptyList();
         }
+    }
+
+
+    private List<Integer> findCommandPositions(final List<String> commandArray, final String commandToSearch) {
+        final List<Integer> positions = new ArrayList<>();
+        for (int i = 0; i < commandArray.size(); i++) {
+            if (commandToSearch.equals(commandArray.get(i))) {
+                positions.add(i);
+            }
+        }
+        return positions;
     }
 
 }
--- test/CommandStringParserTest.java
+++ test/CommandStringParserTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -53,14 +54,57 @@ class CommandStringParserTest {
     }
 
     @Test
+    void equalsSeperation() {
+        final List<String> commands = commandStringParser.parseCommand("11=4");
+        assertThat(commands, contains("11", "=", "4"));
+    }
+
+    @Test
     void separationUsingOperands() {
         final List<String> commands = commandStringParser.parseCommand("11+1+1+d");
         assertThat(commands, contains("11", "+", "1", "+", "1", "+", "d"));
     }
 
     @Test
+    void separationUsingOperandsNegative() {
+        final List<String> commands = commandStringParser.parseCommand("-11 -1 + 1 + d");
+        assertThat(commands, contains("-11", "-1", "+", "1", "+", "d"));
+    }
+
+    @Test
     void supportComments() {
         final List<String> commands = commandStringParser.parseCommand("1 2 + # And so i s t h i s #");
         assertThat(commands, contains("1", "2", "+"));
+    }
+
+    @Test
+    void inAndOutOfComment() {
+        final List<String> commands = commandStringParser.parseCommand("1 2 + # And so i s t h i s # 3");
+        assertThat(commands, contains("1", "2", "+", "3"));
+    }
+
+
+    @Test
+    void onlyComment() {
+        final List<String> commands = commandStringParser.parseCommand("# This is a comment #");
+        assertThat(commands, is(Collections.emptyList()));
+    }
+
+    @Test
+    void multilineComments() {
+        final List<String> commands = commandStringParser.parseCommand("1 2 + # And so i s t h i s ");
+        final List<String> commands2 = commandStringParser.parseCommand("And so i s t h i s #");
+        assertThat(commands, contains("1", "2", "+"));
+        assertThat(commands2, is(Collections.emptyList()));
+    }
+
+    @Test
+    void multilineCommentsNoHashOnLine2() {
+        final List<String> commands = commandStringParser.parseCommand("1 2 + # And so i s t h i s ");
+        final List<String> commands2 = commandStringParser.parseCommand("how about this ");
+        final List<String> commands3 = commandStringParser.parseCommand("And now t h i s # 2");
+        assertThat(commands, contains("1", "2", "+"));
+        assertThat(commands2, is(Collections.emptyList()));
+        assertThat(commands3, contains("2"));
     }
 }
